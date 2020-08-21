0b550230c33ed7437e9da0d8918334906d371362
Brendan <brendan@telus3-desktop.corp.integrateai.net>
2020-08-21 16:18:46 +0000

    bump version

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/DataResponsibly/DataSynthesizer',
-    version='0.1.2+iai.0.0.1',
+    version='0.1.2+iai.0.0.2',
     zip_safe=False,
 )
